5854ce506df359b30dfca0555f7b78675d8afe37
AlbertHuang <r.albert.huang@gmail.com>
2022-06-05 10:27:56 +0800

    remove log

--- internal/pkg/db/mysql.go
+++ internal/pkg/db/mysql.go
@@ -1,10 +1,32 @@
 package db
 
 import (
+	"log"
+	"os"
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func openMysql(dsn string) (*gorm.DB, error) {
-	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	file, err := os.Create("/gorm.log")
+
+	if err != nil {
+		return nil, err
+	}
+
+	newLogger := logger.New(
+		log.New(file, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,   // Slow SQL threshold
+			LogLevel:                  logger.Silent, // Log level
+			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+			Colorful:                  false,         // Disable color
+		},
+	)
+	return gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 }
